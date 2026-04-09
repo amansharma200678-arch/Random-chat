ff68cdbef7a70506a8c2fb1238b9afeb894f3990
amansharma200678-arch <amansharma200678@gmail.com>
2026-04-09 14:56:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -84,3 +84,6 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
